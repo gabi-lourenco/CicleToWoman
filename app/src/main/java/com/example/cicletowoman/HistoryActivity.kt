04686017eea09b80907301b9743849878e701348
Gabriele Lourenço de Jesus <gabrielelourenco000@gmail.com>
2023-06-08 14:26:25 -0300

    Criando tela de historico dos ciclos

---
+++ app/src/main/java/com/example/cicletowoman/HistoryActivity.kt
@@ -0,0 +1,56 @@
+package com.example.cicletowoman
+
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.applikeysolutions.cosmocalendar.selection.SingleSelectionManager
+import com.applikeysolutions.cosmocalendar.utils.SelectionType
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_first_period.*
+import kotlinx.android.synthetic.main.activity_history.*
+import kotlinx.android.synthetic.main.activity_history.cosmo_calendar
+import java.util.*
+
+
+class HistoryActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+
+        setConfigCalendar()
+        setOnClickListeners()
+    }
+
+    private fun setConfigCalendar() {
+        cosmo_calendar.firstDayOfWeek = Calendar.MONDAY
+        cosmo_calendar.selectionType = SelectionType.SINGLE
+    }
+
+    private fun setOnClickListeners() {
+        btnEdit.setOnClickListener {
+            if (cosmo_calendar.selectionManager is SingleSelectionManager) {
+                val rangeManager = cosmo_calendar.selectionManager as SingleSelectionManager
+                if (rangeManager != null) {
+                    showBottomSheetDialog()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Selecione uma data para editar.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_edit_day_history)
+
+        bottomSheetDialog.show()
+    }
+}
